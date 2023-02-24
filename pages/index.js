76a35cb06f1685638c9cb34b6059d0fe32e3969b
Rob Stanford <me@robstanford.com>
2023-02-24 11:00:26 +0000

    initial commit

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
+import LoginBtn from '@/components/login-btn'
 import styles from '@/styles/Home.module.css'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -16,6 +17,7 @@ export default function Home() {
       </Head>
       <main className={styles.main}>
         <div className={styles.description}>
+          <LoginBtn />
           <p>
             Get started by editing&nbsp;
             <code className={styles.code}>pages/index.js</code>
